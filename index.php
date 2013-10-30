2b86ed8442ea44de9b6730009c56bff25c55fdf8
encode101 <rahulmishra.hello@gmail.com>
2013-10-30 18:15:14 +0530

    In Progreess : Basic structure ready

--- index.php
+++ index.php
@@ -56,6 +56,7 @@
     
     <!-- Controls --> 
     <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev"> <span class="icon-prev"></span> </a> <a class="right carousel-control" href="#carousel-example-generic" data-slide="next"> <span class="icon-next"></span> </a> </div>
+  
   <!--	/////////////////	 Banner Ends Here -->
   
   <div class="col-md-4">
@@ -80,7 +81,99 @@
   </div>
   <div class="clearfix"></div>
   <!-- Bottom Panel -->
-  <div class="bottomPanel"></div>
+  <div class="services">
+    <div class="col-md-4 col-sm-4">
+      <div class="element"></div>
+    </div>
+    <div class="col-md-4 col-sm-4">
+      <div class="element"></div>
+    </div>
+    <div class="col-md-4 col-sm-4">
+      <div class="element"></div>
+    </div>
+    <div class="clearfix"></div>
+  </div>
+  <div class="clearfix cus-hspacer-25"></div>
+  <div class="bottomPanel">
+    <div class="col-md-12">
+      <div class="bottom-link">
+        <div class="bottom-in">
+          <ul class="bottom-nav">
+            <li><a class="bg_none" title="Life Insurance" href="/life-insurance/life-insurance-india.aspx"><strong>Life Insurance</strong></a>
+              <ul>
+                <li><a title="Term Insurance" href="/life-insurance/term-insurance-india.aspx">Term Insurance</a></li>
+                <li><a title="Return of Premium" href="/life-insurance/term-insurance-return-of-premium.aspx">Return of Premium</a></li>
+                <li><a title="Hospitalization" href="/hospitalization-insurance/hospitalization-insurance-india.aspx">Hospitalization</a></li>
+                <li><a title="Guaranteed Plan" href="/life-insurance/traditional-plan-india.aspx">Guaranteed Plan</a></li>
+                <li><a title="Money back Plan" href="/life-insurance/moneyback-insurance-india.aspx">Money back Plan</a></li>
+              </ul>
+            </li>
+            <li><a class="bg_none" title="Investment" href="/life-insurance/investment-plans.aspx"><strong>Investment</strong></a>
+              <ul>
+                <li><a title="Pension Plan" href="/life-insurance/pension-insurance-india.aspx">Pension Plans</a></li>
+                <li><a title="Investment Plans" href="/life-insurance/pureinvestment-insurance-india.aspx">Investment Plans</a></li>
+                <li><a title="Child Plans" href="/life-insurance/child-insurance-india.aspx">Child Plans</a></li>
+                <li><a title="ULIP Plans" href="/life-insurance/ulip-plans.aspx">ULIP Plans</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="bottom-nav">
+            <li><a class="bg_none" title="General Insurance" href="/general-insurance-india.aspx"><strong>General Insurance</strong></a>
+              <ul>
+                <li>
+                  <ul>
+                    <li><a title="Health Insurance" href="/health-insurance/health-insurance-india.aspx">Health Insurance</a></li>
+                    <li><a title="Larger Family Health" href="/health-insurance/family-floater-insurance.aspx">Larger Family Health</a></li>
+                    <li><a title="Surgery &amp; Critical Illness" href="/health-insurance/critical-illness-insurance.aspx">Surgery &amp; Critical Illness</a></li>
+                    <li><a title="Senior Citizen Health" href="/health-insurance/senior-citizen-insurance.aspx">Senior Citizen Health</a></li>
+                    <li><a title="Mediclaim" href="/health-insurance/mediclaim-insurance.aspx">Mediclaim</a></li>
+                  </ul>
+                </li>
+                <li><a class="bg_none" title="Motor Insurance" href="/car-insurance/motor-insurance-plans.aspx"><strong>Motor</strong></a>
+                  <ul>
+                    <li><a title="Car Insurance" href="/car-insurance/car-insurance-india.aspx">Car Insurance</a></li>
+                    <li><a title="Two-Wheeler Insurance" href="/twowheeler/twowheeler-insurance-india.aspx">Two-Wheeler Insurance</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="bottom-nav">
+            <li><a class="bg_none" title="Loans" href="/loans/loans.aspx"><strong>Loans</strong></a>
+              <ul>
+                <li><a title="Personal Loan" href="/Personal-Loan/personal-loan-india.aspx">Personal Loan</a></li>
+                <li><a title="Home Loan Transfer" href="http://loans.policybazaar.com/home-loan-transfer/index?utm_source=HLTransfer&amp;utm_term=HLTransfer&amp;utm_campaign=HLTransfer&amp;utm_medium=HLTransfer">Home Loan Transfer</a></li>
+                <li><a title="Car Loan" href="/loans/car-loan.aspx">Car Loan</a></li>
+                <li><a title="Home Loan" href="/home-loans/home-loans-india.aspx">Home Loan</a></li>
+                <li><a title="Credit Cards" href="/creditcards/creditcard-india.aspx">Credit Cards</a></li>
+                <li><a title="Loans Against Property" href="/loans/loan-against-property.aspx">Loans Against Property</a></li>
+                <li><a title="Education Loan" href="/loans/education-loan.aspx">Education Loan</a></li>
+                <li><a title="Tax Calculator" href="/income-tax-calculator/">Tax Calculator</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="bottom-nav">
+            <li><a class="bg_none" title="Travel Insurance" href="/utility/utility.aspx"><strong>Travel &amp; Others</strong></a>
+              <ul>
+                <li><a title="Travel Insurance" href="/travel-insurance/travel-insurance-india.aspx">Travel Insurance</a></li>
+                <li><a title="Mobile Plan" href="/utility/mobile-plans/mobile-plans-india.aspx">Mobile</a></li>
+                <li><a title="Broadband Plan" href="/utility/broadband-plans/broadband-plans-india.aspx">Broadband</a></li>
+                <li><a title="Home Insurance" href="/home-insurance/home-insurance-india.aspx">Home Insurance</a></li>
+                <li><a title="Corporate Insurance" href="/coorporateGMC.aspx">Corporate Insurance</a></li>
+                <li><a title="DTH Plan" href="/utility/Dth-Plans/Dth-Plans-India.aspx">DTH</a></li>
+              </ul>
+            </li>
+          </ul>
+            </li>
+            <!-- <li><a href="/utility/road-assistance-india.aspx" title="Road Assistance India">Road Assistance India</a></li> -->
+          </ul>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+  </div>
+  <div class="clearfix cus-hspacer-25"></div>
+  <div class="footer"> &copy; Copyright 2013. All Rights Reserved. View <a href="disclaimer.php">Disclaimer</a> </div>
   <div class="clearfix cus-hspacer-25"></div>
 </div>
 <script src="js/jquery-1.10.2.js"></script> 
