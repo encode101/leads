bb39e2cd2a10a95584eb9caf855f140778dfd5be
encode101 <rahulmishra.hello@gmail.com>
2013-11-13 03:43:51 +0530

    cleanup

--- process.php
+++
@@ -1,3 +0,0 @@
-<?php
-	echo "<span class='cus-msg-success col-md-10 col-md-offset-1'>Thankyou. Request Received.</span><a href='index.php' class='btn btn-success col-md-8 col-md-offset-2 cus-hspacer-15'>Needs More</a>";
-?>
