dee6a9065912e692065808f656069fd0a2102ade
encode101 <rahulmishra.hello@gmail.com>
2013-12-17 22:46:30 +0530

    removed music folder added ammiabba db path

--- dbconf.php
+++ dbconf.php
@@ -1,10 +1,9 @@
-<?php
+<?php /*
 	mysql_connect('localhost','root','');
 	if(!(mysql_select_db('ammiabba'))){
 		echo("failure");
-	};
-	
-/*
+	}; */
+	/*
 $link = mysql_connect('edugalaorg1.globatmysql.com', 'ammiabbu', 'ammiabbu/@123'); 
 if (!$link) { 
     die('Could not connect: ' . mysql_error()); 
@@ -12,4 +11,10 @@ if (!$link) {
 //echo 'Connected successfully'; 
 mysql_select_db(ammiabbu);
 */
+$link = mysql_connect('supremo.db.11915585.hostedresource.com', 'supremo', 'TesT@123'); 
+if (!$link) { 
+    die('Could not connect: ' . mysql_error()); 
+} 
+//echo 'Connected successfully'; 
+mysql_select_db(ammiabba);
 ?>
